fe0b4ae2e8f0d195e9a08548aaf8adde679b1e63
Leonid <leonid.gaidai1989@gmail.com>
2020-12-26 15:03:20 +0200

    create LazyInst class

---
+++ src/main/java/com/study/create_design/singleton/LazyInst.java
@@ -0,0 +1,24 @@
+package com.study.create_design.singleton;
+
+import java.io.Serializable;
+
+public class LazyInst {
+    // create the instance of the class in synchronized method or synchronized block,
+    // so instance of the class is created when required
+    private static LazyInst lazyInst;
+    // private default constructor
+    private LazyInst(){};
+
+    // getter of this class object
+    public static LazyInst getInstance(){
+        if (lazyInst == null){
+            synchronized (LazyInst.class){
+                if(lazyInst == null){
+                    lazyInst = new LazyInst();
+                }
+            }
+        }
+        return lazyInst;
+    } // End of getInstance()
+
+}

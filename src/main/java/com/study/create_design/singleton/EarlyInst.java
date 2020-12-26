a4a0b0e3c0798f92b99e7791932f7c996da2f638
Leonid <leonid.gaidai1989@gmail.com>
2020-12-26 14:35:09 +0200

    create singleton package with EarlyInst class

---
+++ src/main/java/com/study/create_design/singleton/EarlyInst.java
@@ -0,0 +1,14 @@
+package com.study.create_design.singleton;
+
+public class EarlyInst {
+    // This obj will be loaded in load time
+    private static EarlyInst earlyInst = new EarlyInst();
+    // Default private constructor to instantiate class
+    // and prevent outside creation
+    private EarlyInst(){};
+
+    // getter of object of EarlyInst class
+    public static EarlyInst getEarlyInst() {
+        return earlyInst;
+    }
+}
